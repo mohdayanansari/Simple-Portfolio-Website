933466865404bb4dd8eeee67a326caeef1d214eb
Ayan <1100zaid@gmail.com>
2021-06-07 09:55:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
+
+// cursor
+let mouseCursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", cursor);
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+}
